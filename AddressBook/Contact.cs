7dfffc9c24f453b2795c013b0db37398376b9ce7
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-01 12:38:44 +0530

    Added. Created a contact

---
+++ AddressBook/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        String firstName, lastName, address, city, state, email;
+        String zip, phoneNumber;
+        public Contact(String firstName, String lastName, String address, String city, String state, string zip, string phoneNumber, String email)
+        {
+            
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+
+            Console.WriteLine("Contact is created");
+        }
+    }
+}
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace AddressBook
 {
@@ -6,7 +7,63 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Problem");
+            Console.WriteLine("Welcome to Address Book Problem\n Enter the details of the contact");
+
+            //variables
+
+            String firstName, lastName, address, city, state,email;
+            String zip, phoneNumber;
+
+            Console.WriteLine("Enter First Name: ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name: ");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter Address: ");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter state: ");
+            state = Console.ReadLine();
+
+            Console.WriteLine("Enter zip: ");
+            zip = Console.ReadLine();
+
+            
+            
+            string pattern = @"[0-9]{10}";
+            Regex r = new Regex(pattern);
+
+            Console.WriteLine("Enter phone number: ");
+            phoneNumber = Console.ReadLine();
+
+           if(!r.IsMatch(phoneNumber))
+           {
+              Console.WriteLine("Enter a valid phone number ");
+              System.Environment.Exit(1);
+            }
+           
+            pattern = @"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"
+;
+            Regex r2 = new Regex(pattern);
+
+            Console.WriteLine("Enter email: ");
+            email = Console.ReadLine();
+
+            if (!r2.IsMatch(email))
+            {
+                Console.WriteLine("Enter a valid email");
+                System.Environment.Exit(1);
+            }
+              
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+
+
+
 
         }
     }
